504805c44424da942832412abf7d96ec9d23bd72
svfranklindemo <166366015+svfranklindemo@users.noreply.github.com>
2024-10-21 13:39:21 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -263,8 +263,8 @@ export default async function decorate(block) {
 
   // hamburger for mobile
   const hamburger = document.createElement('div');
-  hamburger?.classList.add('nav-hamburger');
-  hamburger?.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
+  hamburger.classList.add('nav-hamburger');
+  hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
       <span class="nav-hamburger-icon"></span>
     </button>`;
   hamburger?.addEventListener('click', () => toggleMenu(nav, navSections));
